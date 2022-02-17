0a476532c43c71463aae6f86b7f00775e4b4d2df
Joshua <yoonj200@gmail.com>
2022-02-16 18:41:32 -0600

    commit tree 3232df4f7adcf936a304461fedcd001944af63af

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
-
 /* 
-  Write the Big O time complexity of your function here
+  iterate through each number in array
+    for current num, identify complement that adds to target (comp = target - num)
+    iterate through rest of array
+      check if any number is our complement
+      if so, return true
+  if I reach end of array, return false
 */
 
-/* 
-  Add your pseudocode here
-*/
+// ----- Runtime:  O(n^2) => NOT great :( -----
+// function hasTargetSum(array, target) {
+//   for(let i = 0; i < array.length; i++) {
+//     // ----- n steps -----
+//     const complement = target - array[i]
+//     for(let j = i + 1; j < array.length; j++) {
+//       // ----- n + n steps -----
+//       if (array[j] === complement) return true;
+//     }
+//   }
+//   return false;
+// }
 
 /*
-  Add written explanation of your solution here
+create object to keep track of numbers already seen
+iterate through array elements (numbers)
+for current num, identify complement that adds to target (comp = target - num)
+  check if any key on object is complement
+  if so, return true
+if I reach end of array, return false
 */
+
+// ----- Runtime: O(n) => better! :) -----
+function hasTargetSum(array, target) {
+  const seenNumbers = {}
+  for(const number of array) { // ----- n steps -----
+    const complement = target - number
+    if(complement in seenNumbers) return true;
+    seenNumbers[number] = true
+  }
+  return false;
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
